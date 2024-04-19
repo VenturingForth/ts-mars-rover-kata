7b2807c3e331e884d4e87a48bb3b5db58a9e9956
Joshua Robinson <joshua@codeadventure.uk>
2024-04-19 16:19:21 +0100

    Test written for map wrap around

--- __tests__/marsRover.test.ts
+++ __tests__/marsRover.test.ts
@@ -39,6 +39,20 @@ describe("Mars Rover Kata Tests", () => {
     })
     test("A combined movement and rotation instruction should return the correct coordinates and facing of the rover", () => {
         //arrange
-        
+        const rover = new Rover;
+        const instruction: string = "MRMMLML"
+        //act
+        rover.operateMarsRover(instruction);
+        //assert
+        expect(rover.getPosition()).toBe("2:2:W");
+    })
+    test("An instruction that would send the Rover off the 10x10 grid should wrap it back around the map on the opposite side", () => {
+        //arrange
+        const rover = new Rover;
+        const instruction: string = "MMMMMMMMMMMM";
+        //act
+        rover.operateMarsRover(instruction);
+        //assert
+        expect(rover.getPosition()).toBe("0:2:N");
     })
 })
