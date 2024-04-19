7f3c9eaaea0f32c3dd2390e107579204638004c1
Joshua Robinson <joshua@codeadventure.uk>
2024-04-19 15:55:08 +0100

    Passed movement testing

--- __tests__/marsRover.test.ts
+++ __tests__/marsRover.test.ts
@@ -1,12 +1,29 @@
-import { marsRover } from "../src/marsRover";
+import { Rover } from "../src/marsRover";
 
 describe("Mars Rover Kata Tests", () => {
     test("Empty instructions should return the default starting position of the Rover", () => {
         //arrange
+        const rover = new Rover;
         const instruction: string = "";
         //act
-        const result: string = marsRover(instruction);
+        rover.operateMarsRover(instruction);
         //assert
-        expect(result).toBe("");
+        expect(rover.getPosition()).toBe("0:0:N");
+    })
+    test("An instruction of 'M' should move the rover one square in the direction of its facing", () => {
+        //arrange
+        const rover = new Rover;
+        const instruction: string = "M";
+        //act
+        rover.operateMarsRover(instruction);
+        //assert
+        expect(rover.getPosition()).toBe("0:1:N")
+    })
+    test("An instruction of 'L' should rotate the facing of the rover by 90 degrees to the left", () => {
+        //arrange
+        const rover = new Rover;
+        const instruction: string = "L"
+        //act
+        
     })
 })
--- src/marsRover.ts
+++ src/marsRover.ts
@@ -1,4 +1,48 @@
-export function marsRover(instruction: string): string {
-    console.log(instruction);
-    return "0:0:N";
+export class Rover {
+    private facing: string;
+    private posX: number;
+    private posY: number;
+
+    constructor() {
+        this.facing = "N";
+        this.posX = 0;
+        this.posY = 0;
+    }
+
+    move(): void {
+        switch(this.facing){
+            case "N":
+                this.posY++;
+                break;
+            case "E":
+                this.posX++;
+                break;
+            case "S":
+                this.posY--;
+                break;
+            case "W":
+                this.posX--;
+                break;
+        }
+    }
+
+    rotateLeft(): void {
+
+    }
+
+    rotateRight(): void {
+
+    }
+
+    getPosition(): string {
+        return `${this.posX}:${this.posY}:${this.facing}`
+    }
+
+    operateMarsRover(instruction: string): void {
+        for (let i: number = 0; i < instruction.length; i++){
+            if (instruction[i] === "M"){
+                this.move();
+            }
+        }
+    }
 }
